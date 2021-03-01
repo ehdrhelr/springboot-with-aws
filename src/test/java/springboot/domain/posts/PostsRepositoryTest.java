b739f73bebf7d814c78d4cd13197bde61488a34f
Shion <ehdrhelr@gmail.com>
2021-03-01 18:43:41 +0900

    JPA 적용

---
+++ src/test/java/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,49 @@
+package springboot.domain.posts;
+
+import com.shion.book.springboot.domain.posts.Posts;
+import com.shion.book.springboot.domain.posts.PostsRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    void 게시글저장_불러오기() {
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+        System.out.println(postsRepository);
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("aaaa@aaa.aaa")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+
+    }
+}
