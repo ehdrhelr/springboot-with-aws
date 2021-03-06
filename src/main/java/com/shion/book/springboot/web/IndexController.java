e1c207eb4947058cacc2387da8f222688eea666c
Shion <ehdrhelr@gmail.com>
2021-03-07 00:31:44 +0900

    로그인 애노테이션과 세션 설정

--- src/main/java/com/shion/book/springboot/web/IndexController.java
+++ src/main/java/com/shion/book/springboot/web/IndexController.java
@@ -1,7 +1,7 @@
 package com.shion.book.springboot.web;
 
+import com.shion.book.springboot.config.auth.LoginUser;
 import com.shion.book.springboot.config.auth.dto.SessionUser;
-import com.shion.book.springboot.domain.user.User;
 import com.shion.book.springboot.service.posts.PostsService;
 import com.shion.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -20,10 +20,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if (user != null) {
             model.addAttribute("userName", user.getName());
