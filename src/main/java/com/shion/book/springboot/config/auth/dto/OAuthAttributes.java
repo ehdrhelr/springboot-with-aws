a51eeed1e5f23d1dfcd6e58b7adec6ab40ef7661
Shion <ehdrhelr@gmail.com>
2021-03-07 16:36:05 +0900

    네이버 로그인 구현

--- src/main/java/com/shion/book/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/shion/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -24,8 +24,11 @@ public class OAuthAttributes {
         this.picture = picture;
     }
 
-    public static OAuthAttributes of(String registrationId, String userNameAttirbuteName, Map<String, Object> attributes) {
-        return ofGoogle(userNameAttirbuteName, attributes);
+    public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if ("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
+        return ofGoogle(userNameAttributeName, attributes);
     }
 
     private static OAuthAttributes ofGoogle(String userNameAttributesName, Map<String, Object> attributes) {
@@ -35,6 +38,18 @@ public class OAuthAttributes {
                 .picture((String) attributes.get("picture"))
                 .attributes(attributes)
                 .nameAttributeKey(userNameAttributesName)
+                .build();
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profileImage"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
                 .build();
     }
 
